5a851db77373ee7e1d01fe92fce0bb40bc6d1e83
John Garrett <jacgarrett18@gmail.com>
2019-06-07 13:08:57 +0000

    add code in files types: .swift,.storyboard remove code in files types: .swift,.storyboard

---
+++ content.py
@@ -0,0 +1,10 @@
+import git
+from git_contributions_importer import *
+
+root_repo = git.Repo("/Users/jgarrett/Desktop/kabbage-payments-ios")
+# Your mock repo
+fake_repo = git.Repo("/Users/jgarrett/Desktop/kabbage-tracking")
+importer = Importer([root_repo], fake_repo)
+
+importer.set_author(['jacgarrett18@gmail.com', 'jgarrett@kabbage.com'])
+importer.import_repository()
