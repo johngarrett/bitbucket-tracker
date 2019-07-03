ebf81c3d39ef3649757405b147f2d95318ac0eaa
John Garrett <jacgarrett18@gmail.com>
2019-07-03 14:26:55 -0400

    change commit author

---
+++ script.sh
@@ -0,0 +1,17 @@
+ git filter-branch --env-filter '
+    WRONG_EMAIL="jgarrett@kabbage.com"
+NEW_NAME="John Garrett"
+NEW_EMAIL="jacgarrett18@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
