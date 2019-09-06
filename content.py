2d01ce7c730ea6b8f4c9571af23a8111fc2d97cc
John Garrett <jacgarrett18@gmail.com>
2019-09-06 18:28:14 +0000

    add code in files types: .md remove code in files types: .md

--- content.py
+++ content.py
@@ -1,7 +1,7 @@
 import git
 from git_contributions_importer import *
 
-root_repo = git.Repo("/Users/jgarrett/Desktop/kabbagekit")
+root_repo = git.Repo("/Users/jgarrett/Desktop/kabbageuikit")
 # Your mock repo
 fake_repo = git.Repo("/Users/jgarrett/Desktop/kabbage-tracker")
 importer = Importer([root_repo], fake_repo)
