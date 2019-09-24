91c3b37d851a8d0a775fc041a587b4b442a11e6d
John Garrett <jacgarrett18@gmail.com>
2019-09-24 17:14:22 +0000

    add code in files types: .pbxproj,.xcworkspacedata,.plist,.h remove code in files types:

--- content.py
+++ content.py
@@ -1,7 +1,7 @@
 import git
 from git_contributions_importer import *
 
-root_repo = git.Repo("/Users/jgarrett/Desktop/paymentskikit")
+root_repo = git.Repo("/Users/jgarrett/Desktop/PaymentsKit")
 # Your mock repo
 fake_repo = git.Repo("/Users/jgarrett/Desktop/kabbage-tracker")
 importer = Importer([root_repo], fake_repo)
