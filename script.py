4ddb51cc840dedd2d08c3e4dd478140682317efc
John Garrett <jacgarrett18@gmail.com>
2019-10-25 22:39:20 -0400

    update script

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import git
+import os
 from git_contributions_importer import *
 
-root_repo = git.Repo("/Users/jgarrett/Desktop/PaymentsKit")
-# Your mock repo
-fake_repo = git.Repo("/Users/jgarrett/Desktop/kabbage-tracker")
+root_repo = git.Repo(input('Path to the repo to track: ').strip())
+fake_repo = git.Repo(os.getcwd())
 importer = Importer([root_repo], fake_repo)
 
 importer.set_author(['jacgarrett18@gmail.com', 'jgarrett@kabbage.com'])
 importer.import_repository()
+
+os.system('./script.sh')
