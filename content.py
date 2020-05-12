149905a93430b0c982dbb83f57e9a89bd75e0180
John Garrett <jacgarrett18@gmail.com>
2020-05-12 21:10:58 +0000

    add code in files types: .php remove code in files types: .php

--- content.py
+++ content.py
@@ -3,10 +3,13 @@ import os
 from git_contributions_importer import *
 
 root_repo = git.Repo(input('Path to the repo to track: ').strip())
+original_author = input('who was the original author?')
+new_author = input('who is the newauthor?')
+
 fake_repo = git.Repo(os.getcwd())
 importer = Importer([root_repo], fake_repo)
 
-importer.set_author(['jacgarrett18@gmail.com', 'jgarrett@kabbage.com'])
+importer.set_author([new_author, original_author])
 importer.import_repository()
 
-os.system('./script.sh')
+os.system(f'./script.sh {original_author} {new_author}')
